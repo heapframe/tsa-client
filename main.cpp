a862f51f95a065c1317c8fb846442ff5e8ff6fde
axjp <63340001+Alpha-404@users.noreply.github.com>
2026-06-25 13:34:55 +0100

    get TSR, save it + bug fixes

--- main.cpp
+++ main.cpp
@@ -5,9 +5,10 @@
 #include <openssl/x509.h>
 #include <fstream>
 #include <iomanip>
+#include <curl/curl.h>
 
-static TS_REQ *createquery(const unsigned char *digest,
-    const int digest_len, const EVP_MD *md) { //adaptation of create_query from openssl/openssl/blob/master/apps/ts.c:455
+static TS_REQ *createquery(unsigned char *digest,
+                           const int digest_len, const EVP_MD *md) { //adaptation of create_query from openssl/openssl/blob/master/apps/ts.c:455
     TS_REQ *ts_req = nullptr;
     TS_MSG_IMPRINT *msg_imprint = nullptr;
     X509_ALGOR *algo = nullptr;
@@ -24,7 +25,7 @@ static TS_REQ *createquery(const unsigned char *digest,
     algo->parameter = ASN1_TYPE_new();
     algo->parameter->type = V_ASN1_NULL;
     TS_MSG_IMPRINT_set_algo(msg_imprint, algo);
-    TS_MSG_IMPRINT_set_msg(msg_imprint, data, digest_len);
+    TS_MSG_IMPRINT_set_msg(msg_imprint, digest, digest_len);
     TS_REQ_set_msg_imprint(ts_req, msg_imprint);
     TS_REQ_set_cert_req(ts_req, 1);
 
@@ -35,6 +36,42 @@ static TS_REQ *createquery(const unsigned char *digest,
     ASN1_INTEGER_free(nonce_asn1);
 
     return ts_req;
+}
+
+size_t writefunc(void *ptr, size_t size, size_t nmemb, void *userdata)
+{ //adapted from https://stackoverflow.com/questions/2329571/c-libcurl-get-output-into-a-string/61805520#61805520
+    auto *s = static_cast<std::string*>(userdata);
+    s->append(static_cast<char*>(ptr), size * nmemb);
+    return size * nmemb;
+}
+
+CURLcode sendquery(unsigned char *buf, int len, std::string *s) {
+    curl_slist *headers = nullptr;
+    headers = curl_slist_append(headers, "Content-Type: application/timestamp-query");
+    CURLcode ret = curl_global_init(CURL_GLOBAL_ALL);
+
+    if (CURL *curl = curl_easy_init()) {
+        curl_easy_setopt(curl, CURLOPT_BUFFERSIZE, 102400L);
+        curl_easy_setopt(curl, CURLOPT_URL, "https://freetsa.org/tsr");
+        curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 1L);
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, buf);
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE_LARGE, len);
+        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+        curl_easy_setopt(curl, CURLOPT_USERAGENT, "curl/8.2.1");
+        curl_easy_setopt(curl, CURLOPT_MAXREDIRS, 50L);
+        curl_easy_setopt(curl, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_2TLS);
+        curl_easy_setopt(curl, CURLOPT_FTP_SKIP_PASV_IP, 1L);
+        curl_easy_setopt(curl, CURLOPT_TCP_KEEPALIVE, 1L);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writefunc);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, s);
+
+        ret = curl_easy_perform(curl);
+
+        curl_easy_cleanup(curl);
+        curl_slist_free_all(headers);
+    }
+
+    return ret;
 }
 
 int main(const int argc, char *argv[]) {
@@ -65,7 +102,7 @@ int main(const int argc, char *argv[]) {
 
         delete[] memblock;
 
-        TS_REQ *tsq = createquery(hash, sizeof(hash), EVP_sha512());
+        TS_REQ *tsq = createquery(hash, hash_length, EVP_sha512());
         if (tsq == nullptr) {
             std::cerr << "Error: Could not create request object" << std::endl;
             return 1;
@@ -75,22 +112,39 @@ int main(const int argc, char *argv[]) {
         std::string tsq_filename = std::format("{}.tsq", argv[1]);
         unsigned char *buf = nullptr;
 
-        if (int len = i2d_TS_REQ(tsq, &buf); len <= 0) {
+        int len = i2d_TS_REQ(tsq, &buf);
+        if (len <= 0) {
             std::cerr << "Error: Failed to DER encode TSQ" << std::endl;
         } else {
             std::ofstream tsq_output;
-            tsq_output.open (tsq_filename, std::ios::out | std::ios::app | std::ios::binary);
+            tsq_output.open (tsq_filename, std::ios::out | std::ios::binary);
             if (tsq_output.is_open())
             {
                 tsq_output.write(reinterpret_cast<const char*>(buf), len);
                 tsq_output.close();
+                std::cout << "TSQ written to " << tsq_filename << std::endl;
             }
             else std::cout << "Unable to open " << tsq_filename << " for writing" << std::endl;
         }
 
+
+        std::string output;
+        CURLcode tsr = sendquery(buf, len, &output);
+
+        TS_REQ_free(tsq);
         OPENSSL_free(buf);
 
-
+        //save tsr
+        std::string tsr_filename = std::format("{}.tsr", argv[1]);
+        std::ofstream tsr_output;
+        tsr_output.open (tsr_filename, std::ios::out | std::ios::binary);
+        if (tsr_output.is_open())
+        {
+            tsr_output.write(output.c_str(), len);
+            tsr_output.close();
+            std::cout << "TSR written to " << tsr_filename << std::endl;
+        }
+        else std::cerr << "Unable to open " << tsr_filename << " for writing" << std::endl;
     }
     else {
         std::cerr << "Unable to open file";
